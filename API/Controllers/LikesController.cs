7c1083b2849aabf83d9d98e19d277007b3b79771
Keith Dinh <52475783+KeithDinh@users.noreply.github.com>
2021-02-01 16:38:58 -0600

    remove db content

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -13,59 +13,62 @@ namespace API.Controllers
     [Authorize]
     public class LikesController : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
-        private readonly ILikesRepository _likesRepository;
-        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        // private readonly IUserRepository _userRepository;
+        // private readonly ILikesRepository _likesRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public LikesController(IUnitOfWork unitOfWork)
         {
-            _likesRepository = likesRepository;
-            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+            // _likesRepository = likesRepository;
+            // _userRepository.UserRepository = userRepository;
         }
 
         [HttpPost("{username}")]
         public async Task<ActionResult> AddLike(string username)
         {
-            
+
             // get user that current user wants to like
-            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var likedUser = await _unitOfWork.UserRepository.GetUserByUsernameAsync(username);
 
             // get current logged in user
             var sourceUserId = User.GetUserId();
 
             // get current logged in user (sourceUserId) but plus the collection of people who are liked by this users
-            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+            var sourceUser = await _unitOfWork.LikesRepository.GetUserWithLikes(sourceUserId);
 
-            if(likedUser == null) return NotFound();
+            if (likedUser == null) return NotFound();
 
             // prevent user from liking themself.
-            if(sourceUser.UserName == username) return BadRequest("You cannot like yourself");
+            if (sourceUser.UserName == username) return BadRequest("You cannot like yourself");
 
             // check if the sourceUserId already liked the likedUser
-            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
-            if(userLike != null) return BadRequest("You already liked this user");
+            var userLike = await _unitOfWork.LikesRepository.GetUserLike(sourceUserId, likedUser.Id);
+            if (userLike != null) return BadRequest("You already liked this user");
 
             // create a like row/relationship, and add to table
-            userLike = new UserLike{
+            userLike = new UserLike
+            {
                 SourceUserId = sourceUserId,
                 LikedUserId = likedUser.Id
             };
             sourceUser.LikedUsers.Add(userLike);
 
-            if(await _userRepository.SaveAllAsync()) return Ok();
+            if (await _unitOfWork.Complete()) return Ok();
 
             return BadRequest("Failed to like user");
         }
 
         // within current logged-in user, get users who liked or are liked by passing a predicate
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes([FromQuery]LikesParams likesParams)
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes([FromQuery] LikesParams likesParams)
         {
             // set the current user id
             likesParams.userId = User.GetUserId();
 
-            var users = await _likesRepository.GetUserLikes(likesParams);
+            var users = await _unitOfWork.LikesRepository.GetUserLikes(likesParams);
 
             Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
-            
+
             return Ok(users);
         }
     }
