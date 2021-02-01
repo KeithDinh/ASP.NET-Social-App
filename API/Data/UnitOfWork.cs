241186c4817174f915b7b3c2f5e121106bd28718
Keith Dinh <52475783+KeithDinh@users.noreply.github.com>
2021-02-01 16:27:58 -0600

    add untracked unit of work

---
+++ API/Data/UnitOfWork.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using API.Interfaces;
+using AutoMapper;
+
+namespace API.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public UnitOfWork(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public IUserRepository UserRepository => new UserRepository(_context, _mapper);
+
+        public IMessageRepository MessageRepository => new MessageRepository(_context, _mapper);
+
+        public ILikesRepository LikesRepository => new LikesRepository(_context);
+
+        public async Task<bool> Complete() => await _context.SaveChangesAsync() > 0;
+
+        // ChangeTracker is a built-tin method of DbContext class
+        public bool HasChanges() => _context.ChangeTracker.HasChanges();
+    }
+}
